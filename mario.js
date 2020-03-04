595f18d236ba2b305bfd98b7eb5a9f8a35758355
ttitus72372 <tnt2594@gmail.com>
2020-03-04 20:15:40 +0000

    cleaned up extra spaces and comments

--- mario.js
+++ mario.js
@@ -1,23 +1,10 @@
 
 printPyramid(7);
 
-
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
 function printPyramid(height) {
   let x = height;
   let pyramid = ["##"];
   let brick ="#";
-  
   
   while(pyramid.length < x){
 
@@ -25,7 +12,7 @@ function printPyramid(height) {
     let build = current_layer.concat(brick)
     pyramid.push(build);
   }
-  //console.log(pyramid)
+  
   for(i=0;i<pyramid.length;i++){
     let count = height - i - 1
     space = ""
